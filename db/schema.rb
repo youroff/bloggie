175a99cc9f11186c48b868453b7c8ce9f88ab098
ryedeer <bisyarin@mail.ru>
2015-04-05 19:57:38 +0300

    (Oleg B.) Added basic user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328144957) do
+ActiveRecord::Schema.define(version: 20150405082042) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "username"
+    t.string   "password_digest"
   end
 
 end
