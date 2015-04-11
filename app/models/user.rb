13a18fdd49ebeb0f790b0926cefd2c34590247f0
Ivan Yurov <ivan.youroff@gmail.com>
2015-04-11 21:34:06 +0700

    hackaton results

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name, :username, :password
   has_secure_password
-  validates_uniqueness_of :username, case_sensitive: false  
+
+  validates_presence_of :first_name, :last_name, :username
+  validates_uniqueness_of :username, case_sensitive: false
+  
+  has_many :friendships
+  has_many :friends, through: :friendships
+
+  has_many :followings, class_name: Friendship, foreign_key: :friend_id
+  has_many :followers, through: :followings, source: :user
+
 end
