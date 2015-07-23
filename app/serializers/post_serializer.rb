a98823a863f16f5e28323208741bc7a5c132054e
ryedeer <bisyarin@mail.ru>
2015-07-23 21:21:32 +0300

    Minor changes in serializers

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body
+  attributes :id, :title, :body, :user_id
+  has_one :user, key: :user_id, root: :users
+  embed :users, include: true
 end
