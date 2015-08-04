23db703750e67bde979c8d4e743296f80fb45dda
ryedeer <bisyarin@mail.ru>
2015-08-04 19:07:18 +0300

    Starting to implement functions for posts

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -3,13 +3,31 @@ module Api
 		before_action :set_post, only: [:show]
 
 		def index
-			@posts = Post.all
-			render json: @posts
+			case params[:type]
+			when "my_blog"
+					my_blog
+			when "my_feed"
+					my_feed 
+			else
+				@posts = Post.all
+				render json: @posts
+			end
 		end
 
 		def show
 			render json: @post
 		end
+
+		def my_blog
+			posts = Post.where(user_id: current_user.id).order(created_at: :desc)
+			render json: posts, root: "posts"
+		end
+
+		def my_feed
+			posts = Post.feed_of(current_user.id)
+			render json: posts, root: "posts"
+		end
+
 
 		private
 			def set_post
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :user_id
+  attributes :id, :title, :body, :user_id, :created_at
   has_one :user, key: :user_id, root: :user
   embed :user, include: true
 end
